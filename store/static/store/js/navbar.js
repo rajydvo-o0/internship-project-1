aff526f7c9a559f0cdd8154b5dd080d768fdf189
Rajeshvar Yadav <rajydv8887@gmail.com>
2026-06-30 16:45:12 +0530

    Update navbar.js

--- store/static/store/js/navbar.js
+++ store/static/store/js/navbar.js
@@ -1,20 +1,12 @@
 let lastScroll = 0;
-
 window.addEventListener("scroll", () => {
-
     const navbar = document.querySelector(".navbar");
-
     const currentScroll = window.pageYOffset;
-
     if(currentScroll > lastScroll && currentScroll > 80){
-
         navbar.classList.add("hide-navbar");
-
-    }else{
-
-        navbar.classList.remove("hide-navbar");
-
     }
-
+    else{
+        navbar.classList.remove("hide-navbar");
+    }
     lastScroll = currentScroll;
 });
