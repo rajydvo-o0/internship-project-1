24928bba97b04a6b6881b3609c8d2bc458fad7f8
Rajeshvar Yadav <rajydv8887@gmail.com>
2026-06-30 15:49:41 +0530

    Update main.js

--- store/static/store/js/main.js
+++ store/static/store/js/main.js
@@ -3,45 +3,31 @@
 let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
-
     const navbar = document.querySelector(".navbar");
     const categoryBar = document.querySelector(".category-bar");
-
     if (!navbar) return;
-
     let currentScroll = window.pageYOffset;
-
     if (currentScroll > lastScroll) {
-
         navbar.classList.add("hide-navbar");
-
         if (categoryBar) {
             categoryBar.style.top = "0px";
+        }}
+            
+        else {
+            navbar.classList.remove("hide-navbar");
+            if (categoryBar) {
+                categoryBar.style.top = "85px";
+            }
         }
-
-    } else {
-
-        navbar.classList.remove("hide-navbar");
-
-        if (categoryBar) {
-            categoryBar.style.top = "85px";
-        }
-    }
-
     lastScroll = currentScroll;
 });
 
 
 // Toast Notification
-
 function showToast() {
-
     let toast = document.getElementById("toast");
-
     if (!toast) return;
-
     toast.classList.add("show");
-
     setTimeout(() => {
         toast.classList.remove("show");
     }, 3000);
@@ -49,11 +35,8 @@ function showToast() {
 
 
 // Login Popup
-
 function closePopup() {
-
     let popup = document.getElementById("loginPopup");
-
     if (popup) {
         popup.style.display = "none";
     }
@@ -61,10 +44,6 @@ function closePopup() {
 
 
 // Cart Remove Confirmation
-
 function confirmRemove() {
-
-    return confirm(
-        "Are you sure you want to remove this product from the cart?"
-    );
+    return confirm("Are you sure you want to remove this product from the cart?");
 }
